347204d6e1be8d6e8c7f778c9d29e938e78c7547
Loknath Dhar <loknath-dhar@pm.me>
2024-12-28 00:15:49 +0600

    refactored revoke handler

--- handlers/handlerRevoke.go
+++ handlers/handlerRevoke.go
@@ -2,20 +2,21 @@ package handlers
 
 import (
 	"net/http"
-	"strings"
+
+	"github.com/Dhar01/Chirpy/internal/auth"
 )
 
 func (cfg *ApiConfig) HandlerRevoke(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	checkerMethod(w, r, http.MethodPost)
+
+	refreshToken, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "couldn't find token", err)
 		return
 	}
 
-	bearer := r.Header.Get("Authorization")
-	token := strings.TrimSpace(strings.TrimPrefix(bearer, "Bearer"))
-
-	if err := cfg.Queries.RevokeRefreshToken(r.Context(), token); err != nil {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	if err := cfg.Queries.RevokeRefreshToken(r.Context(), refreshToken); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't revoke session", err)
 		return
 	}
 
