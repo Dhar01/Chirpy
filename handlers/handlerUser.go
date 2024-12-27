912e0bbad98bab3c2944e2de3f22922649c0ea7c
Loknath Dhar <loknath-dhar@pm.me>
2024-12-28 00:25:26 +0600

    refactored user handler

--- handlers/handlerUser.go
+++ handlers/handlerUser.go
@@ -3,52 +3,33 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/Dhar01/Chirpy/internal/auth"
 	"github.com/Dhar01/Chirpy/internal/database"
-	"github.com/google/uuid"
 )
 
-type User struct {
-	ID           uuid.UUID `json:"id"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
-	Email        string    `json:"email"`
-	Token        string    `json:"token"`
-	RefreshToken string    `json:"refresh_token"`
-}
-
-type createUserRequest struct {
-	Password string `json:"password"`
-	Email    string `json:"email"`
-	// ExpiresAt string `json:"expires_in_seconds"`
-}
-
 func (cfg *ApiConfig) HandlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
+	checkerMethod(w, r, http.MethodPost)
 
 	var req createUserRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Bad request", http.StatusBadRequest)
+		respondWithError(w, http.StatusInternalServerError, "couldn't decode the request", err)
 		return
 	}
 
 	hashPasswd, err := auth.HashPassword(req.Password)
 	if err != nil {
-		http.Error(w, "can't process password", http.StatusInternalServerError)
+		respondWithError(w, http.StatusInternalServerError, "couldn't hash password", err)
 		return
 	}
 
-	person, err := cfg.Queries.CreateUser(r.Context(), database.CreateUserParams{
+	user, err := cfg.Queries.CreateUser(r.Context(), database.CreateUserParams{
 		Email:          req.Email,
 		HashedPassword: hashPasswd,
 	})
 	if err != nil {
-		http.Error(w, "An error occurred when creating the user", http.StatusInternalServerError)
+		respondWithError(w, http.StatusInternalServerError, "couldn't create user", err)
 		return
 	}
 
@@ -58,23 +39,10 @@ func (cfg *ApiConfig) HandlerCreateUser(w http.ResponseWriter, r *http.Request) 
 	// 	return
 	// }
 
-	token, err := auth.MakeJWT(person.ID, cfg.SecretKey, time.Hour)
-	if err != nil {
-		http.Error(w, "Bad expiration value", http.StatusBadRequest)
-		return
-	}
-
-	user := User{
-		ID:        person.ID,
-		CreatedAt: person.CreatedAt,
-		UpdatedAt: person.UpdatedAt,
-		Email:     person.Email,
-		Token:     token,
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(&user); err != nil {
-		http.Error(w, "An error occurred", http.StatusInternalServerError)
-		return
-	}
+	respondWithJSON(w, http.StatusCreated, User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	})
 }
