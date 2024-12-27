d590c4cacd413b02d33e98a68ae464226aa32429
Loknath Dhar <loknath-dhar@pm.me>
2024-12-28 00:14:53 +0600

    refactored readiness handler

--- handlers/handlerReadiness.go
+++ handlers/handlerReadiness.go
@@ -3,12 +3,9 @@ package handlers
 import "net/http"
 
 func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
+	checkerMethod(w, r, http.MethodGet)
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
